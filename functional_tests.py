e2dc18adc7e8a738c28f78ad90407f19b7fe2485
deus99999 <rudenkooleksei@gmail.com>
2023-10-27 20:04:58 +0300

    First fuctional test was extended. Now is using unittest.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,29 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title()
+class NewVisitorTest(unittest.TestCase):
+    """test of new visitor"""
+
+    def setUp(self):
+        """setup"""
+        self.browser = webdriver.Firefox()
+        
+    def tearDown(self):
+        """demontage"""
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """can begin list and get it later"""
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('To end the test')
+
+# assert 'To-Do' in browser.title, "Browser title was " + browser.title
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
+
+
+# Удовлетворенная она снова ложиться спать
+
