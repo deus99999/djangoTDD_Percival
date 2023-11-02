adfbe9b30185d65fa2744828d124bf416496f21e
deus99999 <rudenkooleksei@gmail.com>
2023-11-02 14:45:14 +0200

    FT checks opportunity for entering element of list now

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+import time
 import unittest
 
 
@@ -14,9 +18,29 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        """can begin list and get it later"""
+        """test: can begin list and get it later"""
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.NAME, 'h1').text
+        self.assertIn('To-Do', header_text)  
+	
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+	    inputbox.get_attribute('placeholder'),
+	    'Enter a to-do item'
+	)
+        inputbox.send_keys('Buy peacock feathers')
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+	
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_element(By.NAME, 'tr')
+        self.assertTrue(
+		any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
+
+
         self.fail('To end the test')
 
 # assert 'To-Do' in browser.title, "Browser title was " + browser.title
@@ -26,4 +50,5 @@ if __name__ == "__main__":
 
 
 # Удовлетворенная она снова ложиться спать
+
 
