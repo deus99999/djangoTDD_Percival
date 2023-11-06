5d8e497ef9d1aea21d7bd022f78a0a55638ed8ca
deus99999 <rudenkooleksei@gmail.com>
2023-11-06 11:33:52 +0200

    HTML of main page is generated from template

--- functional_tests.py
+++ functional_tests.py
@@ -21,7 +21,8 @@ class NewVisitorTest(unittest.TestCase):
         """test: can begin list and get it later"""
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element(By.NAME, 'h1').text
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        print(header_text)
         self.assertIn('To-Do', header_text)  
 	
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
@@ -37,7 +38,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_element(By.NAME, 'tr')
         self.assertTrue(
-		any(row.text == '1: Buy peacock feathers' for row in rows)
+		any(row.text == '1: Buy peacock feathers' for row in rows),
+		'new element did not appear in list '
 		)
 
 
