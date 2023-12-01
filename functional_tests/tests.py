aba6404723530ec0fe73622b50763e95fe461541
deus99999 <rudenkooleksei@gmail.com>
2023-12-01 17:23:24 +0200

    Uses Boostrap for template emproving

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,5 @@
-from django.test import LiveServerTestCase
+#from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -11,7 +12,8 @@ import unittest
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+# class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """test of new visitor"""
 
     def setUp(self):
@@ -20,6 +22,7 @@ class NewVisitorTest(LiveServerTestCase):
         
     def tearDown(self):
         """demontage"""
+        self.browser.refresh()
         self.browser.quit()
 
 	    
@@ -47,7 +50,7 @@ class NewVisitorTest(LiveServerTestCase):
 	
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(
-	    inputbox.getAttribute('placeholder'),
+	    inputbox.get_attribute('placeholder'),
 	    'Enter a to-do item'
 	)
         inputbox.send_keys('Buy peacock feathers')
